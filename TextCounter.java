af3a8cc1959ff9d572148344deb3af5d12f8ffbf
Nitisha16 <78339271+Nitisha16@users.noreply.github.com>
2021-12-21 23:50:44 +0530

    Add files via upload

---
+++ TextCounter.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+    import java.awt.event.*;
+    import java.applet.*;
+    
+    
+    public class TextCounter extends Applet 
+                                      implements ActionListener {
+    
+       TextArea textInput;     // For the user's input text.
+       
+       Label lineCountLabel;   // For displaying the number of lines.
+    
+    
+       public void init() {
+          
+          setBackground(Color.darkGray);
+          
+          textInput = new TextArea();
+          textInput.setBackground(Color.white);
+          
+          Panel south = new Panel();
+          south.setLayout( new GridLayout(4,1,2,2) );
+          
+          Button countButton = new Button("Process the Text");
+          countButton.setBackground(Color.lightGray);
+          countButton.addActionListener(this);
+          south.add(countButton);
+          
+          lineCountLabel = new Label("  Number of lines:");
+          lineCountLabel.setBackground(Color.white);
+          lineCountLabel.setForeground(Color.blue);
+          south.add(lineCountLabel);
+          
+          setLayout( new BorderLayout(2,2) );
+          add(textInput, BorderLayout.CENTER);
+          add(south, BorderLayout.SOUTH);
+          
+       } 
+       
+       
+       public Insets getInsets() {
+          return new Insets(2,2,2,2);
+       }
+       
+    
+       public void actionPerformed(ActionEvent evt) {
+            
+           String text; 
+           
+           int lineCt; 
+          
+           
+           text = textInput.getText();
+           
+        
+           
+           lineCt = 0;
+           for (int i = 0; i < text.length(); i++) {
+              if (text.charAt(i) == '\n')
+                 lineCt++;
+           }
+           
+        
+           
+           lineCountLabel.setText("  Number of Lines:  " + lineCt);
+           
+    
+       }
+       
+
+
+    }
+
+
+
+/*
+
+<applet code ="TextCounter.class" width=800 height=800>
+
+</applet>
+
+*/
